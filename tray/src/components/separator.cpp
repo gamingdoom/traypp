9ba89da8bca901b8ca294989bed4d242733a4f34
Hendrik Lind <34072808+sshcrack@users.noreply.github.com>
2025-08-19 21:41:42 +0200

    Update separator.cpp

--- tray/src/components/separator.cpp
+++ tray/src/components/separator.cpp
@@ -1,4 +1,4 @@
-#include <components/separator.hpp>
-#include <core/entry.hpp>
+#include <tray/components/separator.hpp>
+#include <tray/core/entry.hpp>
 
 Tray::Separator::Separator() : TrayEntry("") {}
